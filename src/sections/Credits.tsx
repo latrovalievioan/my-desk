6ad266a36ec8e5bd622e0c0984dbc9afcb986735
Ioan Latrovaliev <latrovalievioan@gmail.com>
2023-11-04 19:01:29 +0300

    changes to credits section

--- src/sections/Credits.tsx
+++ src/sections/Credits.tsx
@@ -1,22 +1,34 @@
 import styled from "styled-components";
-import { Section } from "../styled/Section"
-import Tilt from 'react-parallax-tilt';
+import { Section } from "../styled/Section";
+import Tilt from "react-parallax-tilt";
 
 const Card = styled(Tilt)`
   // performance
   -webkit-backface-visibility: hidden;
   -webkit-perspective: 1000;
-  -webkit-transform: translate3d(0,0,0);
+  -webkit-transform: translate3d(0, 0, 0);
   -webkit-transform: translateZ(0);
   backface-visibility: hidden;
   perspective: 1000;
-  transform: translate3d(0,0,0);
+  transform: translate3d(0, 0, 0);
   transform: translateZ(0);
 
-  background: rgba(61,61,91, 0.6);
-  background: -moz-linear-gradient(135deg, rgba(61,61,91,0.5) 0%, rgba(73,115,126,0.5) 100%);
-  background: -webkit-linear-gradient(135deg, rgba(61,61,91,0.5) 0%, rgba(73,115,126,0.5) 100%);
-  background: linear-gradient(135deg, rgba(61,61,91,0.5) 0%, rgba(73,115,126,0.5) 100%);
+  background: rgba(61, 61, 91, 0.6);
+  background: -moz-linear-gradient(
+    135deg,
+    rgba(61, 61, 91, 0.5) 0%,
+    rgba(73, 115, 126, 0.5) 100%
+  );
+  background: -webkit-linear-gradient(
+    135deg,
+    rgba(61, 61, 91, 0.5) 0%,
+    rgba(73, 115, 126, 0.5) 100%
+  );
+  background: linear-gradient(
+    135deg,
+    rgba(61, 61, 91, 0.5) 0%,
+    rgba(73, 115, 126, 0.5) 100%
+  );
   backdrop-filter: blur(10px);
   border-radius: 60px;
   padding: 50px;
@@ -39,7 +51,7 @@ const Subject = styled.div`
   text-align: right;
   font-weight: bold;
   flex: 1;
-`
+`;
 
 const Names = styled.div`
   flex: 1;
@@ -64,7 +76,7 @@ export const Credits = () => {
           </Names>
         </Credit>
         <Credit>
-          <Subject>UI/UX</Subject>
+          <Subject>UI/UX ADVISING</Subject>
           <Names>
             <Name>Aneliya Kirkova</Name>
           </Names>
@@ -77,26 +89,15 @@ export const Credits = () => {
           </Names>
         </Credit>
         <Credit>
-          <Subject>BLENDER MODELS</Subject>
-          <Names>
-            <Name>Ioan Latrovaliev</Name>
-            <Name>linus1178</Name>
-            <Name>Tidominer</Name>
-            <Name>MOJackal</Name>
-            <Name>Designed By Jonathan</Name>
-            <Name>DraGGons</Name>
-            <Name>burunduk</Name>
-            <Name>jjmax30</Name>
-          </Names>
-        </Credit>
-        <Credit>
           <Subject>INSPIRATION</Subject>
           <Names>
             <Name>Jesse Zhou</Name>
           </Names>
         </Credit>
-        <Credit>Special thanks to Konstantin Simeonov for being the best mentor!</Credit>
+        <Credit>
+          Special thanks to Konstantin Simeonov for being the best mentor!
+        </Credit>
       </Card>
     </Section>
-  )
-} 
+  );
+};
